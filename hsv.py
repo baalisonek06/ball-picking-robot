d8fe4c6f433c31801013593a21df3ff63e1ef0cc
baalisonek06 <41983265+baalisonek06@users.noreply.github.com>
2019-10-06 22:52:18 +0800

    modify the value of HSV

---
+++ hsv.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+ 
+def nothing(x):
+    pass
+ 
+cap = cv2.VideoCapture(0)
+cv2.namedWindow("Trackbars")
+ 
+cv2.createTrackbar("L - H", "Trackbars", 0, 179, nothing)	#預設初始值
+cv2.createTrackbar("L - S", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("L - V", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
+cv2.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
+ 
+while True:
+    ret , frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)	# BGR   HSV顏色格式轉換
+ 
+    l_h = cv2.getTrackbarPos("L - H", "Trackbars")    #得調整後的	HSV值
+    l_s = cv2.getTrackbarPos("L - S", "Trackbars")
+    l_v = cv2.getTrackbarPos("L - V", "Trackbars")
+    u_h = cv2.getTrackbarPos("U - H", "Trackbars")
+    u_s = cv2.getTrackbarPos("U - S", "Trackbars")
+    u_v = cv2.getTrackbarPos("U - V", "Trackbars")
+ 
+    lower_blue = np.array([l_h, l_s, l_v])
+    upper_blue = np.array([u_h, u_s, u_v])
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)    #(輸入圖像,下限,上限)
+ 
+    result = cv2.bitwise_and(frame, frame, mask=mask)  
+ 
+    cv2.imshow('frame', frame) 
+    cv2.imshow("mask", mask)
+    
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+ 
+cap.release()
+cv2.destroyAllWindows()
